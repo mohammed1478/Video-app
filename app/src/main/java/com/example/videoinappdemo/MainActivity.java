0d3b441566e540944fd12130d053d0279b14eee1
mohammed1478 <chowdhury1478@gmail.com>
2019-06-25 13:52:53 -0400

    1st commit

---
+++ app/src/main/java/com/example/videoinappdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.videoinappdemo;
+
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView vid1 = (VideoView) findViewById(R.id.video1);
+
+        vid1.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.joeroganvideo);
+
+        MediaController mediaController = new MediaController(this); //initialize a media controller to control skipping and pausing etc.
+        mediaController.setAnchorView(vid1); //this connects the media controls to vid1
+        vid1.setMediaController(mediaController); //officially gives media controller to the video
+
+        vid1.start();
+    }
+}
